ade3bcffe20906b93e964c128990db5a185e76c8
Paimo Emmanuel <45436446+PaimoEmmanuel@users.noreply.github.com>
2023-03-13 14:50:09 +0100

    reverse to boilerplate

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,21 +1,9 @@
 import * as React from "react";
-import EventBanner from "../molecules/event-banner";
-import EventDetail from "../molecules/event-detail";
-import Navbar from "../molecules/nav-bar";
-import EventTab from "../organisms/event-tab";
 
 interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
-  return (
-    <div className="home">
-      <Navbar />
-      <EventBanner />
-      <EventDetail />
-      <EventTab />
-      <footer className="footer">Events page via Gratico App</footer>
-    </div>
-  );
+  return <div className="home">Home page</div>;
 };
 
 export default HomePage;
